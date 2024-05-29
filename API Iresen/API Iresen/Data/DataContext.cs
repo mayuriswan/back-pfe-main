4238342c58499bdf062b7c61e8cac3e2495f5221
fatimazahrary <120225272+fatimazahrary@users.noreply.github.com>
2024-05-29 14:08:34 +0100

    add callProjects

--- API Iresen/API Iresen/Data/DataContext.cs
+++ API Iresen/API Iresen/Data/DataContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using API_Iresen;
+using API_Iresen.Models;
 
 namespace API_Iresen.Data
 {
@@ -7,17 +8,17 @@ namespace API_Iresen.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-
         public DbSet<User> Users { get; set; }
-
         public DbSet<Aap> Aaps { get; set; }
+        public DbSet<ProjectCall> ProjectCalls { get; set; } // Ajout du DbSet pour ProjectCall
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder); // Assure que la configuration de base est appelée
+
             modelBuilder.Entity<User>()
                 .HasIndex(u => u.Email)
                 .IsUnique();
         }
-
     }
 }
